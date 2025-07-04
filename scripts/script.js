afeb88111c9c8381a35ad55429f4426f2d69e3e3
h-nakagawa <h-nakagawa@ykc.co.jp>
2025-07-04 14:42:46 +0900

    3rd-commit

--- scripts/script.js
+++ scripts/script.js
@@ -18,7 +18,39 @@ function renderTodos(todos) {
       li.textContent = todo.title;
       ul.appendChild(li);
     });
-  }
+}
+
+
+function addTodo() {
+    const input = document.getElementById('new-todo');
+    const title = input.value.trim();
+    if (!title) return alert('入力してください');
+
+    fetch('https://jsonplaceholder.typicode.com/todos', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ title, completed: false, userId: 1 })
+    })
+    .then(response => {
+        if (!response.ok) throw new Error(`状態コード: ${response.status}`);
+        return response.json();
+    })
+    .then(todo => {
+        renderNewTodo(todo);
+        input.value = '';
+    })
+    .catch(error => console.error('POSTエラー:', error));
+
+}
+
+
+function renderNewTodo(todo) {
+    const ul = document.getElementById('todo-list');
+    const li = document.createElement('li');
+    li.textContent = todo.title;
+    ul.appendChild(li);
+}
   
 
 document.addEventListener('DOMContentLoaded', fetchTodos);
+document.getElementById('add-btn').addEventListener('click', addTodo);
